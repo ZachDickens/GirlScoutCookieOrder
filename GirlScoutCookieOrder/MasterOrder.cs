8974fb1f75a770cd9ad653bc46cb3a797f730e79
ZachDickens <dickens.52@buckeyemail.osu.edu>
2019-02-03 20:35:17 -0500

    More Work

--- GirlScoutCookieOrder/MasterOrder.cs
+++ GirlScoutCookieOrder/MasterOrder.cs
@@ -6,6 +6,49 @@ namespace GirlScoutCookieOrder
 {
     public class MasterOrder
     {
-        public List<Orders> MasterOrder { get; set; }
+        public List<CookieOrder> orders = new List<CookieOrder>();
+
+        public MasterOrder()
+        {
+
+        }
+
+
+        public void AddOrder(CookieOrder theOrder)
+        {
+            orders.Add(theOrder);
+        }
+        public int GetTotalBoxes()
+        {
+            int numberofboxes = 0;
+            for(int i =0; i < orders.Count; i++)
+            {
+                numberofboxes += orders[i].NumberOfBoxesPurchased;
+            }
+            return numberofboxes;
+        }
+        public void RemoveVariety(string variety)
+        {
+            for (int i = 0; i < orders.Count; i++)
+            {
+                if (orders[i].variety.Equals(variety))
+                {
+                    orders.RemoveAt(i);
+                }
+            }
+        }
+        public int GetVarietyBoxes(string variety)
+        {
+            return 0;
+        }
+        public void ShowOrder()
+        {
+
+        }
+
+
     }
+
+
+
 }
